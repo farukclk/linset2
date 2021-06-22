dacd295a22aa1255612e120d3dbaf5582ae869de
farukclk <39220698+farukclk@users.noreply.github.com>
2021-06-22 10:24:25 -0400

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,7 +1,7 @@
-if ! pwd | /home:then
+if ! pwd | grep /home:then
 echo " linset2 klosorunu /home dizinin altına tasıyın [!]"
 
 else
 sudo apt update
 sudo apt install wifite hcxdumptool php aircrack-ng python3 php-cli 
-
+fi
