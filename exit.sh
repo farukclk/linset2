9b3e757f868ff5bb3f6362c37a906e262cab0c70
farukclk <39220698+farukclk@users.noreply.github.com>
2021-06-22 10:17:47 -0400

    Update exit.sh

--- exit.sh
+++ exit.sh
@@ -1,15 +1,14 @@
+ifconfig wlan0 down
 service hostapd stop
 service dnsmasq stop
 service apache2 stop
-ifconfig wlan0 down
-killall dnsmasq hostapd
+killall dnsmasq 
+killall hostapd
 echo "exiting is succesfull"
 echo " " > files/data/status.txt
 
 airmon-ng stop wlan1mon
 
 killall aireplay-ng
-
-#./airmon check kill
 
 echo "exiting is succesfull"
