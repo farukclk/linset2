e7267a530b13a0e33058b4dd64ca90c9ff4d43b1
farukclk <39220698+farukclk@users.noreply.github.com>
2021-06-22 10:15:06 -0400

    Update run.sh

--- hotspot/run.sh
+++ hotspot/run.sh
@@ -1,7 +1,7 @@
-# https yazmadigin surece tum http siteleri yonlendiriyor
-#google u da yonlendiriyor
-service apache2 start
-ifconfig wlan0 192.168.1.1 netmask 255.255.255.0
-dnsmasq -C dnsmasq.conf
+ifconfig wlan0 down
+iptables --flush
+echo 1 > /proc/sys/net/ipv4/ip_forward
 hostapd hostapd.conf -B
-
+ifconfig wlan0 192.168.1.1 netmask 255.255.255.0
+service apache2 start
+dnsmasq -C dnsmasq.conf
